7174fe209a77f3a5da6aad828e2d61b17d639663
Vanity <ktkyletran@gmail.com>
2022-05-28 15:35:49 -0500

    added rect button

--- components/Button.js
+++ components/Button.js
@@ -21,10 +21,25 @@ export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
   )
 };
 
-export const RectButton = () => {
+export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
   return (
-    <View>
-      <Text>Rectangle Button</Text>
-    </View>
+    <TouchableOpacity style={{
+      backgroundColor: COLORS.primary,
+      borderRadius: SIZES.extraLarge,
+      minWidth: minWidth,
+      padding: SIZES.small,
+      ...props,
+    }}
+    onPress={handlePress}
+    >
+      <Text style={{
+        fontWeight: '500',
+        fontSize: fontSize,
+        textAlign: 'center',
+        color: COLORS.white,
+      }}>
+        Place a bid
+      </Text>
+    </TouchableOpacity>
   )
 };
